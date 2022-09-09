affe77589e7d33031fac722f1a9e38e9813396f9
aman <samanjot94@gmail.com>
2022-09-09 16:31:41 +0530

    completed

---
+++ script.js
@@ -0,0 +1,8 @@
+var button=document.querySelector('#blogs');
+var button2=document.querySelector('#btn1');
+var div=document.querySelector('.backend');
+var div2=document.querySelector('.prj-img');
+button2.addEventListener('click',()=>div2.scrollIntoView({behavior:"smooth"}));
+button.addEventListener('click',()=>div.scrollIntoView({behavior:"smooth"}));
+
+
